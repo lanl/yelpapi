43aed998e3ad16c4fc1c15f59085fee6fec685d7
Geoffrey Fairchild <gfairchild@lanl.gov>
2016-05-10 11:57:00 -0600

    use new string formatting syntax

--- examples/examples.py
+++ examples/examples.py
@@ -22,7 +22,7 @@ yelp_api = YelpAPI(args.consumer_key, args.consumer_secret, args.token, args.tok
     Example search by location text and term. Take a look at https://www.yelp.com/developers/documentation/v2/search_api for
     the various options available.
 """
-print('***** 5 best rated ice cream places in Austin, TX *****\n%s\n' % "yelp_api.search_query(term='ice cream', location='austin, tx', sort=2, limit=5)")
+print('***** 5 best rated ice cream places in Austin, TX *****\n{}\n'.format("yelp_api.search_query(term='ice cream', location='austin, tx', sort=2, limit=5)"))
 response = yelp_api.search_query(term='ice cream', location='austin, tx', sort=2, limit=5)
 pprint(response)
 
@@ -32,7 +32,7 @@ print('\n-----------------------------------------------------------------------
     Example search by bounding box and category. See https://www.yelp.com/developers/documentation/v2/all_category_list for an official
     list of Yelp categories. The bounding box definition comes from http://isithackday.com/geoplanet-explorer/index.php?woeid=12587707.
 """
-print('***** 5 bike rentals in San Francisco county *****\n%s\n' % "yelp_api.search_query(category_filter='bikerentals', bounds='37.678799,-123.125740|37.832371,-122.356979', limit=5)")
+print('***** 5 bike rentals in San Francisco county *****\n{}\n'.format("yelp_api.search_query(category_filter='bikerentals', bounds='37.678799,-123.125740|37.832371,-122.356979', limit=5)"))
 response = yelp_api.search_query(category_filter='bikerentals', bounds='37.678799,-123.125740|37.832371,-122.356979', limit=5)
 pprint(response)
 
@@ -41,7 +41,7 @@ print('\n-----------------------------------------------------------------------
 """
     Example business query. Look at https://www.yelp.com/developers/documentation/v2/business for more information.
 """
-print("***** selected reviews for Amy's on 6th St. *****\n%s\n" % "yelp_api.business_query(id='amys-ice-creams-austin-3')")
+print("***** selected reviews for Amy's on 6th St. *****\n{}\n".format("yelp_api.business_query(id='amys-ice-creams-austin-3')"))
 response = yelp_api.business_query(id='amys-ice-creams-austin-3')
 pprint(response)
 
@@ -51,7 +51,7 @@ print('\n-----------------------------------------------------------------------
     Example phone search query. Look at https://www.yelp.com/developers/documentation/v2/phone_search for
     more information.
 """
-print("***** search for business by phone number *****\n%s\n" % "yelp_api.phone_search_query(phone='+13193375512')")
+print('***** search for business by phone number *****\n{}\n'.format("yelp_api.phone_search_query(phone='+13193375512')"))
 response = yelp_api.phone_search_query(phone='+13193375512')
 pprint(response)
 
@@ -60,7 +60,7 @@ print('\n-----------------------------------------------------------------------
 """
     Example erronious search query.
 """
-print('***** sample erronious search query *****\n%s\n' % "yelp_api.search_query(term='ice cream', location='austin, tx', sort=3)")
+print('***** sample erronious search query *****\n{}\n'.format("yelp_api.search_query(term='ice cream', location='austin, tx', sort=3)"))
 try:
     # sort can only take on values 0, 1, or 2
     yelp_api.search_query(term='ice cream', location='austin, tx', sort=3)
@@ -72,7 +72,7 @@ print('\n-----------------------------------------------------------------------
 """
     Example erronious business query.
 """
-print('***** sample erronious business query *****\n%s\n' % "yelp_api.business_query(id='fake-business')")
+print('***** sample erronious business query *****\n{}\n'.format("yelp_api.business_query(id='fake-business')"))
 try:
     yelp_api.business_query(id='fake-business')
 except YelpAPI.YelpAPIError as e:
